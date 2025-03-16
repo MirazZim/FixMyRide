6ca63e865cc4897077b2c0e7150e44502d5ffa64
MirazZim <miraz.zim.38@gmail.com>
2025-03-16 14:46:03 +0600

    Successfully created service details bannner

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased  max-w-screen-2xl mx-auto`}
       >
         <Navbar />
         {children}
--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -1,8 +1,34 @@
+import dbConnect, { collectionNamesObject } from '@/library/dbConnect/dbConnect';
+import { ObjectId } from 'mongodb';
+import Image from 'next/image';
 import React from 'react'
 
 export default async function ServiceDetailsPage({ params }) {
-    const p = await params
-  return (
-    <div>Service Detail : {p.id}</div>
-  )
+    const p = await params;
+    /* DB Connection */
+    const serviceCollection = dbConnect(collectionNamesObject.servicesCollection)
+    const data = await serviceCollection.findOne({ _id: new ObjectId(p.id) })
+    /* ........................................................ */
+    return (
+        <div>
+            {/* Banner Section */}
+            <section className='flex justify-center '>
+                <figure className=' mt-2 relative '>
+                    <Image src={"/assets/images/checkout/checkout.png"}
+                        alt={"banner"}
+                        width={1350}
+                        height={200} />
+                    <div className='transparent-layer overlay-bg absolute w-full h-full  top-0'>
+                        <div className='w-full h-full flex justify-center items-center'>
+                            <div>
+                                <h1 className='text-4xl font-bold text-white'>Service Details</h1>
+                            </div>
+                        </div>
+                    </div>
+                </figure>
+            </section>
+            <h1>Service Detail : {p.id}</h1>
+
+        </div>
+    )
 }
